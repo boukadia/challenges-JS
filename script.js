394fcffb8825be36f9d502320bd6e63ceb639030
boukadia <mustapha.boukadia97@gmail.com>
2024-12-04 09:36:42 +0100

    partie1-2-3-commit

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 "use strict"
 let modifier=document.querySelectorAll("button")[2]
 let delet=document.querySelector(".delete")
-let add=document.querySelector(".add")[2]
+let add=document.querySelector(".add")
 let navbar=document.querySelector(".navbar")
-// let carte=document.querySelector(".carte")
-// add.onclick=function(){
+let position=document.querySelectorAll('.position .coordonnes')[0]
+console.log(position.textContent)
+delet.onclick=function(){
+document.body.style.backgroundColor="white"
+alert("hello world!")
+}
+
+add.addEventListener("click",function(){
+    document.body.style.backgroundColor="red"
+    
+   
+})
+
+function show(event){
+   let  positionVertical=event.clientX
+    let  positionHorizental=event.clientY
+     let text= "coord du X: " + positionVertical + `<br>`+ " coord du Y : " + positionHorizental;
+      position.innerHTML = text;
+     
+    
+}
 
 
-// }
 

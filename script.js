b3e72dd2afc3a5aae0f66d492ce06aa55b035f62
boukadia <mustapha.boukadia97@gmail.com>
2024-12-04 08:50:07 +0100

    first-commit

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict"
+let modifier=document.querySelectorAll("button")[2]
+let delet=document.querySelector(".delete")
+let add=document.querySelector(".add")[2]
+let navbar=document.querySelector(".navbar")
+// let carte=document.querySelector(".carte")
+// add.onclick=function(){
+
+
+// }
+
